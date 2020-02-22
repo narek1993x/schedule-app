099721617c22e1ae02b35e87e80bdab66b3550d8
Narek Khachatryan <narek1993c@gmail.com>
2020-02-22 18:41:27 +0400

    Update pwa configs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Todo",
+    themeColor: "#161e26",
+    msTileColor: "#161e26",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#161e26"
+  }
 };
