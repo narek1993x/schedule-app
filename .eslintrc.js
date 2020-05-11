d5213ca656f56bb4b6adddd51fb9d6de4b0b2ed6
Narek Khachatryan <narek1993c@gmail.com>
2020-05-11 20:40:39 +0400

    Change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         htmlWhitespaceSensitivity: "ignore"
       }
     ],
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-unused-vars": "warn",
     indent: ["off", 4]
