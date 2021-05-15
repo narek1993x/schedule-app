c0434865b3c84e5de102bfb5a75daa10bbf56bee
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-05-15 19:31:34 +0400

    Update firebase init app keys

--- src/firebase.js
+++ src/firebase.js
@@ -9,6 +9,9 @@ if (!firebase.apps.length) {
     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
     databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
   });
 }
 
