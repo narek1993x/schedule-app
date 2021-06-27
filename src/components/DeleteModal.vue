f3b71e68f42a90c5e7b62f8330e2ce39369bdd72
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-06-27 16:17:31 +0400

    Create reusable delete modal

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -1,10 +1,8 @@
 <template>
   <Modal :width="400" :dark="dark" :visible="show" :onClose="onClose">
     <v-card>
-      <v-card-title class="headline">Delete Event</v-card-title>
-      <v-card-text>
-        Are you sure you want to permanently delete this event?
-      </v-card-text>
+      <v-card-title class="headline">Delete {{ type }}</v-card-title>
+      <v-card-text> Are you sure you want to permanently delete this {{ type }}? </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="blue darken-1" text @click="onClose">
@@ -20,7 +18,7 @@
 
 <script>
 export default {
-  props: ["show", "dark", "loading", "onClose", "onDelete"],
+  props: ["show", "type", "dark", "loading", "onClose", "onDelete"],
 };
 </script>
 
