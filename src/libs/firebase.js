2db090b11ccaf032c324f932b593ebde406104c0
Narek Khachatryan <Narek_Khachatryan1@epam.com>
2021-10-27 11:08:46 +0400

    Add measurementId and pass app to analytics

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -3,17 +3,22 @@ import "firebase/database";
 import "firebase/firestore";
 import "firebase/auth";
 import "firebase/firebase-messaging";
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.VUE_APP_FIREBASE_APP_ID,
-  });
+  const app = firebase.initializeApp(firebaseConfig);
+  firebase.analytics(app);
 }
 
 export default firebase;
